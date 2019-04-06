063956b2f3f5bb424b794a5a9f8aa72a15a8b0af
Lypivets <lipavetsalexandr@gmail.com>
2019-04-06 19:05:51 +0300

    Added liquibase

--- src/main/java/com/lesson/config/DatabaseConfig.java
+++ src/main/java/com/lesson/config/DatabaseConfig.java
@@ -1,9 +1,15 @@
 package com.lesson.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import liquibase.integration.spring.SpringLiquibase;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.persistence.EntityManagerFactory;
 
@@ -12,9 +18,30 @@ import javax.persistence.EntityManagerFactory;
  */
 @Configuration
 @EntityScan("com.lesson")
+@EnableConfigurationProperties(LiquibaseProperties.class)
 public class DatabaseConfig {
+
+    @Autowired
+    private LiquibaseProperties liquibaseProperties;
+
     @Bean
     public SessionFactory sessionFactory(EntityManagerFactory emf) {
         return emf.unwrap(SessionFactory.class);
+    }
+
+    @Bean
+    @Primary
+    public SpringLiquibase liquibase(HikariDataSource dataSource){
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog(liquibaseProperties.getChangeLog());
+        liquibase.setContexts(liquibaseProperties.getContexts());
+        liquibase.setDefaultSchema(liquibaseProperties.getDefaultSchema());
+        liquibase.setDropFirst(liquibaseProperties.isDropFirst());
+        liquibase.setShouldRun(liquibaseProperties.isEnabled());
+        liquibase.setLabels(liquibaseProperties.getLabels());
+        liquibase.setChangeLogParameters(liquibaseProperties.getParameters());
+        liquibase.setRollbackFile(liquibaseProperties.getRollbackFile());
+        return liquibase;
     }
 }
